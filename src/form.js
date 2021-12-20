db593a16a0a40d87691023be29dcecf57c747b4e
Jeanette Yue <yue.jeanette@gmail.com>
2021-12-20 00:15:45 -0800

    Refactor using singleton pattern

--- src/form.js
+++ src/form.js
@@ -1,48 +1,51 @@
-// endpoint for formspark service
-const ENDPT = "https://submit-form.com/ylFjWaig";
-const ERROR_MSG = "Unable to send";
+// Add event listener to form submittals
+(function () {
+  // endpoint for formspark service
+  const ENDPT = "https://submit-form.com/ylFjWaig";
+  const ERROR_MSG = "Unable to send";
 
-// global form elements
-const form = document.querySelector(".contact__form");
-const toast = form.querySelector(".form__toast-wrapper");
+  // global form elements
+  const form = document.querySelector(".contact__form");
+  const toast = form.querySelector(".form__toast-wrapper");
 
-// post form data to formspark endpoint
-async function postData(data) {
-  fetch(ENDPT, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then(() => toggleToast(toast))
-    .catch(() => handleError(toast));
-}
+  // post form data to formspark endpoint
+  async function postData(data) {
+    fetch(ENDPT, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then(() => toggleToast(toast))
+      .catch(() => handleError(toast));
+  }
 
-// get field data from form and return it in structure of post body
-function getFormData(form) {
-  const email = form.querySelector("#email");
-  const msg = form.querySelector("#message");
-  return { email: email.value, msg: msg.value };
-}
+  // get field data from form and return it in structure of post body
+  function getFormData(form) {
+    const email = form.querySelector("#email");
+    const msg = form.querySelector("#message");
+    return { email: email.value, msg: msg.value };
+  }
 
-function toggleToast(toast) {
-  toast.classList.toggle("form__toast-wrapper--visible");
-}
+  function toggleToast(toast) {
+    toast.classList.toggle("form__toast-wrapper--visible");
+  }
 
-function handleError(toast) {
-  const msg = toast.querySelector(".form__toast");
-  msg.innerText = ERROR_MSG;
-}
+  function handleError(toast) {
+    const msg = toast.querySelector(".form__toast");
+    msg.innerText = ERROR_MSG;
+  }
 
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  // send message to endpoint and display and alert user of success or fail
-  await postData(getFormData(form));
-  // hide toast after 2 seconds and clear form
-  setTimeout(() => {
-    toggleToast(toast);
-    form.reset();
-  }, 3000);
-});
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    // send message to endpoint and display and alert user of success or fail
+    await postData(getFormData(form));
+    // hide toast after 2 seconds and clear form
+    setTimeout(() => {
+      toggleToast(toast);
+      form.reset();
+    }, 3000);
+  });
+})();
