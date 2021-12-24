9c2f84e381d8033c7152acb4a5189569065d8850
Jeanette Yue <yue.jeanette@gmail.com>
2021-12-24 05:44:05 -0800

    Fix error handling logic

--- src/form.js
+++ src/form.js
@@ -1,12 +1,14 @@
-// Add event listener to form submittals
+// Add event listener to form submissions
 (function () {
   // endpoint for formspark service
   const ENDPT = "https://submit-form.com/ylFjWaig";
   const ERROR_MSG = "Unable to send";
+  const SUCCESS_MSG = "Message sent";
 
   // global form elements
   const form = document.querySelector(".contact__form");
   const toast = form.querySelector(".form__toast-wrapper");
+  const msg = toast.querySelector(".form__toast");
 
   // post form data to formspark endpoint
   async function postData(data) {
@@ -18,8 +20,8 @@
       },
       body: JSON.stringify(data),
     })
-      .then(() => toggleToast(toast))
-      .catch(() => handleError(toast));
+      .then((res) => handleFormToast(res.status === 200))
+      .catch(() => handleFormToast(false));
   }
 
   // get field data from form and return it in structure of post body
@@ -29,22 +31,24 @@
     return { email: email.value, msg: msg.value };
   }
 
-  function toggleToast(toast) {
+  function toggleToast() {
     toast.classList.toggle("form__toast-wrapper--visible");
   }
 
-  function handleError(toast) {
-    const msg = toast.querySelector(".form__toast");
-    msg.innerText = ERROR_MSG;
+  //show message depending on success of posting to formspark
+  function handleFormToast(status) {
+    const _msg = status ? SUCCESS_MSG : ERROR_MSG;
+    msg.innerText = _msg;
+    toggleToast();
   }
 
   form.addEventListener("submit", async (e) => {
     e.preventDefault();
     // send message to endpoint and display and alert user of success or fail
     await postData(getFormData(form));
-    // hide toast after 2 seconds and clear form
+    // hide toast after 3 seconds and clear form
     setTimeout(() => {
-      toggleToast(toast);
+      toggleToast();
       form.reset();
     }, 3000);
   });
