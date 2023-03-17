dcfffb49669faa3d5ba9b312f6187ad3c30b2b2b
Jeanette Yue <yue.jeanette@gmail.com>
2023-03-16 19:35:01 -0700

    Add placeholder image and lazyload gifs

---
+++ src/lazyload.js
@@ -0,0 +1,26 @@
+(function () {
+  const imgList = document.querySelectorAll("#projects .card__img");
+
+  [...imgList].map((img) => {
+    const gif = img.querySelector("img");
+    const gifSrc = gif.getAttribute("data-portfolio-src");
+    console.log(img);
+    const OPTIONS = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0,
+    };
+
+    const observer = new IntersectionObserver(handleIntersect, OPTIONS);
+    observer.observe(gif);
+
+    function handleIntersect(entries) {
+      entries.forEach((entry) => {
+        const { target, isIntersecting } = entry;
+        if (target === gif) {
+          gif.setAttribute("src", gifSrc);
+        }
+      });
+    }
+  });
+})();
